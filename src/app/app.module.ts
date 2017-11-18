6e6a63abb5ecda03d603aabc3af2cfe0be68d511
Jesus Torres <jesustorres38@hotmail.com>
2017-11-18 19:51:43 +0100

    creating files and add Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+//Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Main Component
 import { AppComponent } from './app.component';
+
+//Material Components
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+//Here we import all the elements of angular material that we are going to use
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatCheckboxModule],
+})
+
+export class MaterialModule { }
