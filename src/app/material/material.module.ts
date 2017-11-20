cfb7cd1c85b9659e8707a1ec815645e83afcbfe7
Jesus Torres <jesustorres38@hotmail.com>
2017-11-20 22:00:08 +0100

    navbar component and icons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 //Here we import all the elements of angular material that we are going to use
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
 })
 
 export class MaterialModule { }
