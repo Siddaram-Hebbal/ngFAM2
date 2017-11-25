c06c8e4e79c8617a060121ac1d0043af39f0a07a
Jesus Torres <jesustorres38@hotmail.com>
2017-11-25 19:42:22 +0100

    menu navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,12 +6,13 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material';
 import {MatIconModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material';
 
 
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatTabsModule, MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatTabsModule, MatButtonModule, MatToolbarModule, MatIconModule],
 })
 
 export class MaterialModule { }
