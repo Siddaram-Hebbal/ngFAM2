de00ac0745f8128e75bfefea6254bf61f60e7b50
Jesus Torres <jesustorres38@hotmail.com>
2017-12-01 20:29:57 +0100

    form ready now working on function to register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,6 @@
 import { FireService } from './../services/fire.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-declare var $ :any;
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,26 +11,18 @@ export class RegisterComponent implements OnInit {
 
   
   public registerForm: FormGroup;
+  
 
   constructor(private auth: FireService) { }
 
   ngOnInit() {
-    // Inicializamos el select (necesario con materilize)
-    $('select').material_select();
-
     //creamos la instancia del reactive form
     this.registerForm = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl(),
-      role: new FormControl()
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      admin: new FormControl(null, Validators.required)
     });
-
-
-  }
-
-  mensaje(data){
-    console.log(data);
   }
 
 }
